4b4d6822999b1c186381545928f7075504cce8cd
youyi.qin <youyi.qin@shihengtech.com>
2022-04-21 00:54:20 +0800

    add: :memo: new content

--- services/unsplash.ts
+++ services/unsplash.ts
@@ -6,7 +6,6 @@ export const searchImgByKeyword = async (keyword: string = "code"): Promise<IWal
     const url = `https://api.unsplash.com/search/photos/?client_id=${
       process.env.UNSPLASH_ID ?? "tYopoedda78BuuvIBpnag9T2EjGHX2ZPCcw4MRCk_9U"
     }&query=${keyword}&lang=zh&w=1920`;
-    console.log(url);
     const {data} = axios.get(url);
     return data;
   } catch (error) {
